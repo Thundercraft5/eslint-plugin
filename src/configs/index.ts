f255fcdb0af45bfe7b719aedb0201b8a27c68ae7
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2022-05-20 17:51:17 -0400

    Updates

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,9 +1,9 @@
 export const
-	{ default: base } = await import("./.eslintrc.base.json", { assert: { type: "json" } }),
-	{ default: esnext } = await import("./.eslintrc.esnext.json", { assert: { type: "json" } }),
-	{ default: node } = await import("./.eslintrc.node.json", { assert: { type: "json" } }),
-	{ default: modules } = await import("./.eslintrc.modules.json", { assert: { type: "json" } }),
-	{ default: react } = await import("./.eslintrc.react.json", { assert: { type: "json" } }),
-	{ default: reactESNext } = await import("./.eslintrc.react-esnext.json", { assert: { type: "json" } }),
-	{ default: typescript } = await import("./.eslintrc.typescript.json", { assert: { type: "json" } }),
+	{ default: base } = await import("./base.eslintrc.json", { assert: { type: "json" } }),
+	{ default: esnext } = await import("./esnext.eslintrc.json", { assert: { type: "json" } }),
+	{ default: node } = await import("./node.eslintrc.json", { assert: { type: "json" } }),
+	{ default: modules } = await import("./modules.eslintrc.json", { assert: { type: "json" } }),
+	{ default: react } = await import("./react.eslintrc.json", { assert: { type: "json" } }),
+	{ default: reactESNext } = await import("./react-esnext.eslintrc.json", { assert: { type: "json" } }),
+	{ default: typescript } = await import("./typescript.eslintrc.json", { assert: { type: "json" } }),
 	_ = undefined;
