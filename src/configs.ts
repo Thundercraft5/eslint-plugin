30f4374de1f6d996d8f008e7e30b659fd8831040
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2022-06-08 20:46:16 -0400

    Add overrides to config

---
+++ src/configs.ts
@@ -0,0 +1,7 @@
+export { default as base } from "../configs/base.eslintrc.json" assert { type: "json" };
+export { default as esnext } from "../configs/esnext.eslintrc.json" assert { type: "json" };
+export { default as modules } from "../configs/modules.eslintrc.json" assert { type: "json" };
+export { default as node } from "../configs/node.eslintrc.json" assert { type: "json" };
+export { default as react } from "../configs/react.eslintrc.json" assert { type: "json" };
+export { default as reactESNext } from "../configs/react-esnext.eslintrc.json" assert { type: "json" };
+export { default as typescript } from "../configs/typescript.eslintrc.json" assert {type: "json" };
--- src/configs/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as base } from "./base.eslintrc.json" assert { type: "json" };
-export { default as esnext } from "./esnext.eslintrc.json" assert { type: "json" };
-export { default as modules } from "./modules.eslintrc.json" assert { type: "json" };
-export { default as node } from "./node.eslintrc.json" assert { type: "json" };
-export { default as react } from "./react.eslintrc.json" assert { type: "json" };
-export { default as reactESNext } from "./react-esnext.eslintrc.json" assert { type: "json" };
-export { default as typescript } from "./typescript.eslintrc.json" assert { type: "json" };
