22dc7fd23a791b5a0d01a9b65f1043ab76df34a9
Thundercraft5 <thundercraft5@protonmail.com>
2025-04-22 12:36:40 -0400

    Fix

---
+++ src/eslint-plugin-promise.d.ts
@@ -0,0 +1,17 @@
+declare module "eslint-plugin-promise" {
+
+}
+
+declare module "eslint-plugin-markdown" {
+
+}
+
+declare module "eslint-plugin-node" {
+
+}
+
+declare module "eslint-plugin-comments" {   }
+
+declare module "eslint-plugin-import" { 
+    
+  }
